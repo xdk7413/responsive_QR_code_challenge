75b0802033cb8f68c3a446660f2365efc030cd86
Jason H <jhsucode@gmail.com>
2022-09-09 12:34:50 -0700

    yee

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -3,7 +3,16 @@ import styles from "./Card.module.scss";
 export default function Card() {
   return (
     <div className={styles.container}>
-      <div>Placeholder</div>
+      <div className={styles.container_card}>
+        <div className={styles.image_container}>
+          <img src="/image-qr-code.png" alt="" />
+        </div>
+        <h1>Improve your front-end skills by building projects</h1>
+        <p>
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
     </div>
   );
 }
